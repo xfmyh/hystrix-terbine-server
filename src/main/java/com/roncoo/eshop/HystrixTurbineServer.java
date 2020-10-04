f9f2c1d1bb73a0ffe6bb3751b55bbe0dfce18fc8
myh <myh514@icloud.com>
2020-10-04 23:24:27 +0800

    demo

--- src/main/java/com/roncoo/eshop/HystrixTurbineServer.java
+++ src/main/java/com/roncoo/eshop/HystrixTurbineServer.java
@@ -2,10 +2,12 @@ package com.roncoo.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
+@EnableHystrixDashboard
 public class HystrixTurbineServer {
     public static void main(String[] args) {
         SpringApplication.run(HystrixTurbineServer.class,args);
